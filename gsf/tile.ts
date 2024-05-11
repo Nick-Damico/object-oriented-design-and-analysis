7de3118ed39efbe9751a6e9290388e41e20e0b20
Nick-Damico <nickalan82@icloud.com>
2024-05-11 14:16:20 -0400

    feat: place unit within Tile

--- gsf/tile.ts
+++ gsf/tile.ts
@@ -1,11 +1,19 @@
+import { iUnit } from './unit'
+
 export interface Tile {
   type: string
+  unit?: iUnit
 }
 
 export class TerrainTile {
   type: string
+  unit: iUnit
 
   constructor() {
     this.type = 'Terrain'
+  }
+
+  setUnit(unit: iUnit): void {
+    this.unit = unit
   }
 }
