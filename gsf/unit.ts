1b9bffa0f3c25625360e3d3aa670c0c9ed498ce5
Nick-Damico <nickalan82@icloud.com>
2024-05-12 09:20:55 -0400

    refactor: use base class instead of interface

--- gsf/unit.ts
+++ gsf/unit.ts
@@ -1,9 +1,5 @@
-export interface iUnit {
-  _type: string
-}
-
-export class Unit implements iUnit {
-  _type: string
+export default class Unit {
+  private _type: string
 
   constructor(type: string) {
     this._type = type
