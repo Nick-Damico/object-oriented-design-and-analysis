26deeaa08833e1948145bcfaa0bcc5743f2fee17
Nick-Damico <nickalan82@icloud.com>
2024-05-19 11:49:15 -0400

    feat: remove unit(s) from board tile

--- gsf/tile.ts
+++ gsf/tile.ts
@@ -1,26 +1,31 @@
 import Unit from './unit'
 
 export class Tile {
-  private _type: string
   // private _units: Map<string, Unit[]>
   private _units: Unit[]
 
+  // Does Order matter on units?
+  // We might want to implement with a Queue or Stack
   constructor() {
-    this._type = 'Terrain'
     this._units = []
   }
 
-  getType(): string {
-    return this._type
-  }
-
-  public setUnit(unit: Unit): void {
+  public addUnit(unit: Unit): void {
     this._units.push(unit)
   }
 
-  getUnits(): Unit[] {
+  public getUnits(): Unit[] {
     return this._units
   }
 
+  public removeUnit(unit: Unit): void {
+    let unitIndex = this.getUnits().indexOf(unit)
+    if (unitIndex === -1) return
+
+    this.getUnits().splice(unitIndex, 1)
+  }
+
+  public removeUnits(): void {
+    this.getUnits().forEach((unit) => this.removeUnit(unit))
   }
 }
