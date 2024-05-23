7681817bd8fb04bd38642b0b3f93cf441bcf5e46
Nick-Damico <nickalan82@icloud.com>
2024-05-23 09:15:10 -0400

    chore(comment): removed

--- gsf/board.ts
+++ gsf/board.ts
@@ -6,12 +6,6 @@ export class Board {
   private _height: number
   private _board: Tile[][]
 
-  /**
-   * What is the unit of measurement?
-   * Do we need to support different measurement systems?
-   * Do we handle conversion?
-   * How large is a tile?
-   */
   constructor(width: number, height: number) {
     this._width = width
     this._height = height
