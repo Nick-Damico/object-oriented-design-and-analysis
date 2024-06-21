c678f73679a119046a08ab418805462675e67a84
Nick-Damico <nickalan82@icloud.com>
2024-06-21 14:29:49 -0400

    feat(subway): add support for Subways

--- RouteFinder/Subway.ts
+++ RouteFinder/Subway.ts
@@ -1,5 +1,9 @@
+import Station from './Station'
+
 export default class Subway {
   private _name: string
+  private _stations: Station[] // Make a LinkedList
+  private _stationsCount: number
 
   constructor(name: string) {
     this._name = name
@@ -7,5 +11,12 @@ export default class Subway {
 
   getName(): string {
     return this._name
+  }
+
+  addStation(name: string): void {
+    let newStation = new Station(name)
+
+    this._stations.push(newStation)
+    ++this._stationsCount
   }
 }
