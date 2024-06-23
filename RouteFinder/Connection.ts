f2627a4b2f3d2592276c5e2192e2cff5a9f0021e
Nick-Damico <nickalan82@icloud.com>
2024-06-23 08:35:30 -0400

    feat: completed Connection

--- RouteFinder/Connection.ts
+++ RouteFinder/Connection.ts
@@ -1,11 +1,25 @@
-export default class Connection {
-  private _name: string
+import Station from './Station'
 
-  constructor(name: string) {
-    this._name = name
+export default class Connection {
+  private _station1: Station
+  private _station2: Station
+  private _lineName: string
+
+  constructor(station1: Station, station2: Station, lineName: string) {
+    this._station1 = station1
+    this._station2 = station2
+    this._lineName = lineName
   }
 
-  getName(): string {
-    return this._name
+  getStation1(): Station {
+    return this._station1
+  }
+
+  getStation2(): Station {
+    return this._station2
+  }
+
+  getLineName(): string {
+    return this._lineName
   }
 }
